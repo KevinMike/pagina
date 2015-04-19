d35c28851ebbeeaa234c53ab6dc229415942eff0
Kevin Mike Herrera Vega <kevin_22894@hotmail.com>
2015-04-19 13:25:21 -0500

    GALERIA DE FOTOS DINAMICA
    El archivos gitignore esta sin contenido, por lo que el proyecto
    completo esta subido al repositorio, es decir /verdor y /node_modules,
    que este proyecto siga cresciendo ;)

--- resources/views/fotos.blade.php
+++ resources/views/fotos.blade.php
@@ -1,6 +1,19 @@
 @extends('master')
+
 @section('cabecera')
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
 @endsection
 @section('contenido')
-fotos
+		<div class="subtitulo" style=" display: block; width: 100%; border-bottom: 5px solid black;">
+			<h2 >GALERIA DE FOTOS DINÁMICA</h2>
+		</div>
+		<br>
+		<img class="imagen" src="images/1.jpg" alt="" />
+		<img class="imagen" src="images/2.jpg" alt="" />
+		<img class="imagen" src="images/3.jpg" alt="" />
+		<img class="imagen" src="images/4.jpg" alt="" />
+		<img class="imagen" src="images/5.jpg" alt="" />
 @endsection
---
+++ resources/views/practicas.blade.php
@@ -0,0 +1,80 @@
+@extends('master')
+@section('contenido')
+<h4 style="text-align: center;">Prácticas Pre- Profesionales en el INEI</h4>
+<br>
+Objetivos:
+<br>
+<ul>
+<li>Brindar orientación y capacitación técnica y profesional a estudiantes y egresados de Universidades e Institutos Superiores del país.</li>
+<li>Proporcionar recursos humanos en forma temporal a las Unidades Orgánicas del INEI, a fin de contribuir al normal desarrollo de las actividades institucionales.</li>
+</ul>
+<br>
+DURACIÓN
+<br>
+Las Prácticas Pre-Profesionales tendrán una duración mínima de 04 meses a razón de 04 horas diarias, de lunes a viernes en el turno de mañana de 09.00 a 13.00 hrs o tarde de 14.00 a 18.00hrs según los requerimientos del área solicitante. Las prácticas podrán ser prorrogadas hasta por dos (02) meses más, a solicitud del Director de la Unidad o Área donde se desarrollen las prácticas
+<br>
+BENEFICIOS
+<br>
+Durante el desarrollo de sus prácticas Pre-Profesionales:
+<br>
+<ul>
+<li>Conocer e interiorizarse con los conceptos y definiciones de las principales variables  loop{p}
+{p} económicas, financieras, sociales y demográficas de las diferentes investigaciones estadísticas que ejecuta el INEI</li>
+<li>Tener el derecho de acceso a la información estadística de las investigaciones que realiza el INEI, para apoyar sus trabajos de investigación, monografías y tesis</li>
+</li>Acceder al beneficio de beca integral en los derechos de enseñanza en tres (03) cursos de capacitación ordinarios que desarrolla la Escuela Nacional de Estadística e Informática del INEI, durante un año (incluido su período de prácticas pre-profesionales), tales como SPSS, STATA, Análisis de Indicadores y Elaboración de Reportes Socioeconómicos, Construcción e Interpretación de Indicadores Económicos y Sociales, Diseño y manejo de Base de datos Ofimáticas con Excel y Access, Administración de Proyectos con MS Project, Macros en Excel para el análisis de la información estadística, Herramientas Ofimáticas, entre otros.</li>
+</ul>
+Al culminar sus prácticas Pre-Profesionales el practicante recibe: 
+<br>
+Constancia o Certificado de Prácticas Pre-Profesionales, suscrito por el INEI. Recibir la Constancia o Certificado está en función a las horas de prácticas realizadas en el INEI.
+<br>
+REQUISITOS PARA POSTULACIÓN
+<br>
+Podrán postular a las Prácticas PRE-Profesionales que convoque la ENEI:
+<br>
+<ul>
+<li>Los egresados universitarios.</li>
+<li>Los estudiantes universitarios que hayan aprobado como mínimo el 8vo. Ciclo Académico.</li>
+<li>Los egresados de los Institutos Superiores Tecnológicos, con una formación mínima de seis (06) semestres académicos.</li>
+</ul>
+<br>
+ÁREAS DE PRACTICAS PRE PROFESIONALES
+<br>
+El programa comprende las siguientes áreas:
+<br>
+<ul>
+<li>Economía.</li>
+<li>Estadística.</li>
+<li>Administración.</li>
+<li>Contabilidad.</li>
+<li>Desarrollo de Sistemas.</li>
+<li>Soporte Técnico.</li>
+<li>Secretariado.</li>
+<li>Ingeniería de Sistemas.</li>
+<li>Ingeniería Geográfica y otras áreas afines.</li>
+</ul> 
+INSCRIPCIÓN
+<br>
+Los interesados deben descargar la Solicitud y la Ficha de Inscripción de la web y presentar la documentación completa en la Escuela Nacional de Estadística e Informática (ENEI), sito en Pasaje Hernán Velarde N° 285 Lima (Refer.: Cuadra 1 de la Av. Arequipa), telef. 433-3127 / 332-4650. Horario de Atención: Lunes a Viernes de 08:30 a 16.30 horas.
+Documentos para presentar al momento de solicitar Practicas - Preprofesionales
+<br>
+<ul>
+<li>Solicitud para realizar prácticas pre profesionales. (Descargar)
+Ficha de Inscripción del postulante a prácticas pre profesionales.(Descargar)</li>
+<li>Carta de Presentación o Constancia de Estudios expedida por la respectiva Facultad, para los estudiantes universitarios del 8° al 10° ciclo.</li>
+<li>Constancia de egresado o grado de bachiller de la especialidad de Economía, Contabilidad, Administración, Estadística, Ingenierías y carreras afines</li>
+<li>Currículum Vitae sin documentar.</li>
+<li>Fotocopia del DNI.</li>
+<li>Una fotografía tamaño carné (esta fotografía deberá ser pegada en la parte superior derecha de la  Ficha de Inscripción)</li>
+ </ul>
+ <br>
+CERTIFICACIÓN
+<br>
+Se expedirá el Certificado de Prácticas pre-profesionales consignando el número de horas de práctica y la calificación obtenida, a la presentación del informe final de prácticas. En el término de 05 días de finalizado el periodo de prácticas, las Áreas o Unidades Orgánicas remitirán a la ENEI, el expediente del informe final de los practicantes adjuntando:
+<br>
+<ul>
+<li>Oficio del Director de la Unidad Orgánica respectiva.</li>
+<li>El Informe de Prácticas pre-profesionales, elaborado por el practicante, según el Anexo. (Esquema_Informe)</li>
+<li>El Formato "Esquema de calificación de Prácticas Pre Profesionales" (Formato_Calificación), firmado por el jefe inmediato y visado por el Director Nacional o Técnico respectivo.</li>
+<li>Reporte de asistencia diaria.(Formato_Asistencia)</li>
+</ul>
+@endsection
