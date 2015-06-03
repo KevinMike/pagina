6903cc7e1434cfb2006129070b1bbccff5c985eb
Kevin Mike Herrera Vega <kevin_22894@hotmail.com>
2015-06-03 12:04:18 -0500

    NAVBAR TERMINADO
    Falta arreglar el header y luego wualaa

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,59 +4,89 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="witdh=device-width, intial-scale=1, maximun-scale=1">
 	<title>ENEI: Escuela del Instituto Nacional de Ingeniería en Informática y Sistemas</title>
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/app.css">
+	
 	<!-- JQUERY-->
 	<script src="js/jquery-2.1.3.min.js"></script>
 	<script src="js/jquery-ui-1.11.3/jquery-ui.js"></script>
 	<link rel="stylesheet" href="js/jquery-ui-1.11.3/jquery-ui.css">
+	<!-- FIN JQUERY -->
+	
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css">
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<!-- FIN BOOTSTRAP --> 
+
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<!--<link rel="stylesheet" href="css/app.css">-->
 	@yield('cabecera')
+
 </head>
 <body onload="initialize()">
 	<header>
 			<div class="container">
 				<div class="row">
-					<div class="titulo col-lg-6 col-md-12 ">
+					<img class="col-lg-2 col-md-6 " id="img2" src="img/inei.jpg" alt="" height="100px">
+					<div class="col-lg-8 col-md-0 titulo">
 						<p valign="middle">ESCUELA NACIONAL DE ESTADISTICA E INFORMÁTICA<br>Sede Tacna</p>
 					</div>
-					<div class="logo col-lg-6 col-md-12 " >
+					<img class="col-lg-2 col-md-6 " id="img1"src="img/logo.jpg" alt="" height="100px">
+					<!--<div class="logo col-lg-6 col-md-12 " >
 						<img id="img1"src="img/logo.jpg" alt="" height="100px">
 						<img id="img2" src="img/inei.jpg" alt="" height="100px">
-					</div>
+					</div>-->
 				</div>
 			</div>
 	</header>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="{{ url('/') }}" id="inicio">Inicio</a>
-		      <!--<a class="navbar-brand" href="#">Brand</a>-->
-		    </div>
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="{{ url('/') }}" id="inicio">ENEI</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="items nav navbar-nav ">
 				<li class="active"><a href="{{ url('/') }}" id="inicio">Inicio</a></li>
 				<li><a href="{{ url('/informacion') }}" id="quienes_somos">Quienes Somos</a></li>
 				<li><a href="{{ url('/cursos') }}" id ="cursos">Cursos</a></li>
-				<!--<li><a href="" id="herramientas">Herramientas</a></li>-->
+				<li><a href="{{ url('/fotos') }}" id="fotos">Fotos</a></li>
+				<li><a href="{{ url('/contacto')}}" id="contactanos">Contáctanos</a></li>
+				<li><a target="_blank" href="http://localhost/moodle" id="aula_virtual">Aula Virtual</a></li>
+				<li><a href="{{ url('/mapa') }}" id="mapa">Mapa</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+	<!--<nav class="">
+			<ul class="">
+				<li class=""><a href="{{ url('/') }}" id="inicio">Inicio</a></li>
+				<li><a href="{{ url('/informacion') }}" id="quienes_somos">Quienes Somos</a></li>
+				<li><a href="{{ url('/cursos') }}" id ="cursos">Cursos</a></li>
 				<li><a href="{{ url('/fotos') }}" id="fotos">Fotos</a></li>
 				<li><a href="{{ url('/contacto')}}" id="contactanos">Contáctanos</a></li>
 				<li><a target="_blank" href="http://localhost/moodle" id="aula_virtual">Aula Virtual</a></li>
 				<li><a href="{{ url('/mapa') }}" id="mapa">Mapa</a></li>
 			</ul>
-		</div>
-	</nav>
+	</nav>-->
+	<div class="separador"> </div>
 	<section>
 	<!--<div class="container">-->
-		<section id="principal" >
+		<article id="principal" >
 			@yield('contenido')
-		</section>
+		</article>
 		<aside>
 			<div class="subsection1">
 				<div class="cabecera">
